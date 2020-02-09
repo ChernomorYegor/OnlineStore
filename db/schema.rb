1298dba904aca2afe59276dc150f891ec55f7c7b
ChernomorYegor <chernomor.yegor@gmail.com>
2020-02-10 02:42:59 +0300

    Modeling Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_235818) do
+ActiveRecord::Schema.define(version: 2020_02_09_200210) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
