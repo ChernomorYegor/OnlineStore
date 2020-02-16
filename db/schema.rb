987a9271e4162109ad1a77c3307dd3d8101b6ef8
ChernomorYegor <chernomor.yegor@gmail.com>
2020-02-16 04:03:57 +0300

    fix price and notice

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_200210) do
+ActiveRecord::Schema.define(version: 2020_02_15_234503) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "price"
+    t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
