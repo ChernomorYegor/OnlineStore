2a5db2488b70b27ecb3c42dda9f551d8374f2928
ChernomorYegor <chernomor.yegor@gmail.com>
2020-02-16 16:57:11 +0300

    validates new items and create cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+	validates :name, uniqueness: true
+	validates :name, :description, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_234503) do
+ActiveRecord::Schema.define(version: 2020_02_16_113409) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
